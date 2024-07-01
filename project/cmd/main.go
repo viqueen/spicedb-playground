347867ed7491dab6057d92952e8cfbc874c9be84
viqueen <hasnae.rehioui@safetyculture.io>
2024-07-01 10:58:53 +1000

    [noissue] welcome write schema

--- project/cmd/main.go
+++ project/cmd/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	pb "github.com/authzed/authzed-go/proto/authzed/api/v1"
 	"github.com/authzed/authzed-go/v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"log"
+	"os"
+	"path/filepath"
 )
 
 const spicedbEndpoint = `localhost:50051`
@@ -14,7 +19,34 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
+	setupSchema(client)
 	setupOrg(client)
+}
+
+func setupSchema(client *authzed.Client) {
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("failed to get working directory: %v", err)
+	}
+
+	schemaFile := filepath.Join(wd, "cmd", "schema.zed")
+	content, err := os.ReadFile(schemaFile)
+
+	if err != nil {
+		log.Fatalf("failed to read schema file: %v", err)
+	}
+
+	schema := string(content)
+	fmt.Printf("schema: %s\n", schema)
+
+	request := &pb.WriteSchemaRequest{
+		Schema: schema,
+	}
+	response, err := client.WriteSchema(context.Background(), request)
+	if err != nil {
+		log.Fatalf("failed to write schema: %v", err)
+	}
+	fmt.Printf("response: %v\n", response)
 }
 
 func setupOrg(client *authzed.Client) {
